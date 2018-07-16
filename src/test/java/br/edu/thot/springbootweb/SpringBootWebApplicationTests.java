04116a3cb49a74c922bcb9906ea67a4e0aa3296e
Lincoln John <lincolnjohn@gmail.com>
2018-07-15 21:27:24 -0300

    Projeto criado

---
+++ src/test/java/br/edu/thot/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.thot.springbootweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
